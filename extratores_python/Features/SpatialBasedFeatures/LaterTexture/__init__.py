9aa09692ff83762dc1a61eef8f8e2caf85fd5d89
clementinojrNotebook <juninhoclementino45@gmail.com>
2021-10-03 19:00:13 -0300

    Excluindo arquivos não utilizados

--- extratores_python/Features/SpatialBasedFeatures/LaterTexture/__init__.py
+++
@@ -1,9 +0,0 @@
-from .hos import hos_features, plot_sinogram
-from .lbp import lbp_features, lbp_features_no_mask
-from .glszm import glszm_features
-from .lpq import lpq_features
-from .haralick import haralick_features
-
-__all__ = ['hos_features','plot_sinogram',
-           'lbp_features', 'lbp_features_no_mask',
-           'glszm_features', 'lpq_features', 'haralick_features']
--- extratores_python/Features/SpatialBasedFeatures/LaterTexture/haralick.py
+++
@@ -1,6 +0,0 @@
-import mahotas as mt
-
-def haralick_features(image):
-    textures = mt.features.haralick(image)
-    ht_mean = textures.mean(axis=0)
-    return ht_mean
